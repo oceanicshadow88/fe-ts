c7538c12c8c8bdcb52cb5cd8ac5beb887abfda71
kitman <kitman200220022002@gmail.com>
2025-04-26 10:47:36 +1000

    remove unused file

--- abc.sh
+++
@@ -1,5 +0,0 @@
-PAYLOAD='{"user": "TheUnstoppables2019:ATBBKxmdFWwpnNVyvcF8KYZJbTxy98EC005C"}'
-
-RESPONSE=`curl --request GET -H "Content-Type:application/json" https://api.bitbucket.org/2.0/repositories/010001/fe.techscrum/pullrequests/293	 --data "${PAYLOAD}"`
-
-jq/help
