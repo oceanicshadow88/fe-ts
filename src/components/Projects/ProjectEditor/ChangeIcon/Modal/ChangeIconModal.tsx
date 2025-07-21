351073c5a49d2d38c69680fb8ee394a84ac5c90d
Chelsea.Y <yangqingyan0@gmail.com>
2025-07-21 16:12:12 +1000

    refactor: remove repetitive code

--- src/components/Projects/ProjectEditor/ChangeIcon/Modal/ChangeIconModal.tsx
+++ src/components/Projects/ProjectEditor/ChangeIcon/Modal/ChangeIconModal.tsx
@@ -1,107 +1,101 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { FiArrowLeft } from 'react-icons/fi';
 import { RiMoreFill } from 'react-icons/ri';
 import styles from './ChangeIconModal.module.scss';
 import uploadImage from '../../../../../assets/uploadImage.png';
 import { upload } from '../../../../../api/upload/upload';
 
+const icons = [
+  {
+    id: 1,
+    photo:
+      'https://010001.atlassian.net/secure/viewavatar?size=xxxlarge@2x&avatarId=10411&avatarType=project'
+  },
+  {
+    id: 2,
+    photo:
+      'https://010001.atlassian.net/secure/viewavatar?size=xxxlarge@2x&avatarId=10400&avatarType=project'
+  },
+  {
+    id: 3,
+    photo:
+      'https://010001.atlassian.net/secure/viewavatar?size=xxxlarge@2x&avatarId=10401&avatarType=project'
+  },
+  {
+    id: 4,
+    photo:
+      'https://010001.atlassian.net/secure/viewavatar?size=xxxlarge@2x&avatarId=10402&avatarType=project'
+  },
+  {
+    id: 5,
+    photo:
+      'https://010001.atlassian.net/secure/viewavatar?size=xxxlarge@2x&avatarId=10403&avatarType=project'
+  },
+  {
+    id: 6,
+    photo:
+      'https://010001.atlassian.net/secure/viewavatar?size=xxxlarge@2x&avatarId=10404&avatarType=project'
+  },
+  {
+    id: 7,
+    photo:
+      'https://010001.atlassian.net/secure/viewavatar?size=xxxlarge@2x&avatarId=10405&avatarType=project'
+  },
+  {
+    id: 8,
+    photo:
+      'https://010001.atlassian.net/secure/viewavatar?size=xxxlarge@2x&avatarId=10406&avatarType=project'
+  },
+  {
+    id: 9,
+    photo:
+      'https://010001.atlassian.net/secure/viewavatar?size=xxxlarge@2x&avatarId=10407&avatarType=project'
+  },
+  {
+    id: 10,
+    photo:
+      'https://010001.atlassian.net/secure/viewavatar?size=xxxlarge@2x&avatarId=10408&avatarType=project'
+  },
+  {
+    id: 11,
+    photo:
+      'https://010001.atlassian.net/secure/viewavatar?size=xxxlarge@2x&avatarId=10409&avatarType=project'
+  },
+  {
+    id: 12,
+    photo:
+      'https://010001.atlassian.net/secure/viewavatar?size=xxxlarge@2x&avatarId=10410&avatarType=project'
+  },
+  {
+    id: 13,
+    photo:
+      'https://010001.atlassian.net/secure/viewavatar?size=xxxlarge@2x&avatarId=10415&avatarType=project'
+  },
+  {
+    id: 14,
+    photo:
+      'https://010001.atlassian.net/secure/viewavatar?size=xxxlarge@2x&avatarId=10412&avatarType=project'
+  },
+  {
+    id: 15,
+    photo:
+      'https://010001.atlassian.net/secure/viewavatar?size=xxxlarge@2x&avatarId=10413&avatarType=project'
+  },
+  {
+    id: 16,
+    photo:
+      'https://010001.atlassian.net/secure/viewavatar?size=xxxlarge@2x&avatarId=10414&avatarType=project'
+  }
+];
+
 interface IModalProps {
   shown: boolean;
   close: () => void;
   uploadSuccess: (data: any) => void;
 }
-
 export default function ChangeIconModal({ shown, close, uploadSuccess }: IModalProps) {
-  const icons = [
-    {
-      id: 1,
-      photo:
-        'https://010001.atlassian.net/secure/viewavatar?size=xxxlarge@2x&avatarId=10411&avatarType=project'
-    },
-    {
-      id: 2,
-      photo:
-        'https://010001.atlassian.net/secure/viewavatar?size=xxxlarge@2x&avatarId=10400&avatarType=project'
-    },
-    {
-      id: 3,
-      photo:
-        'https://010001.atlassian.net/secure/viewavatar?size=xxxlarge@2x&avatarId=10401&avatarType=project'
-    },
-    {
-      id: 4,
-      photo:
-        'https://010001.atlassian.net/secure/viewavatar?size=xxxlarge@2x&avatarId=10402&avatarType=project'
-    },
-    {
-      id: 5,
-      photo:
-        'https://010001.atlassian.net/secure/viewavatar?size=xxxlarge@2x&avatarId=10403&avatarType=project'
-    },
-    {
-      id: 6,
-      photo:
-        'https://010001.atlassian.net/secure/viewavatar?size=xxxlarge@2x&avatarId=10404&avatarType=project'
-    },
-    {
-      id: 7,
-      photo:
-        'https://010001.atlassian.net/secure/viewavatar?size=xxxlarge@2x&avatarId=10405&avatarType=project'
-    },
-    {
-      id: 8,
-      photo:
-        'https://010001.atlassian.net/secure/viewavatar?size=xxxlarge@2x&avatarId=10406&avatarType=project'
-    },
-    {
-      id: 9,
-      photo:
-        'https://010001.atlassian.net/secure/viewavatar?size=xxxlarge@2x&avatarId=10407&avatarType=project'
-    },
-    {
-      id: 10,
-      photo:
-        'https://010001.atlassian.net/secure/viewavatar?size=xxxlarge@2x&avatarId=10408&avatarType=project'
-    },
-    {
-      id: 11,
-      photo:
-        'https://010001.atlassian.net/secure/viewavatar?size=xxxlarge@2x&avatarId=10409&avatarType=project'
-    },
-    {
-      id: 12,
-      photo:
-        'https://010001.atlassian.net/secure/viewavatar?size=xxxlarge@2x&avatarId=10410&avatarType=project'
-    },
-    {
-      id: 13,
-      photo:
-        'https://010001.atlassian.net/secure/viewavatar?size=xxxlarge@2x&avatarId=10415&avatarType=project'
-    },
-    {
-      id: 14,
-      photo:
-        'https://010001.atlassian.net/secure/viewavatar?size=xxxlarge@2x&avatarId=10412&avatarType=project'
-    },
-    {
-      id: 15,
-      photo:
-        'https://010001.atlassian.net/secure/viewavatar?size=xxxlarge@2x&avatarId=10413&avatarType=project'
-    },
-    {
-      id: 16,
-      photo:
-        'https://010001.atlassian.net/secure/viewavatar?size=xxxlarge@2x&avatarId=10414&avatarType=project'
-    }
-  ];
-  const firstFiveIcons = icons.slice(0, 5);
-  const fiveIcons = firstFiveIcons.map((firstFiveIcon) => (
-    <li key={firstFiveIcon.id}>
-      <img src={firstFiveIcon.photo} alt="icon" />
-    </li>
-  ));
-
-  const uploadFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const [iconCollection, setIconCollection] = useState(false);
+  const handleUploadFile = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (!e.target.files) {
       return;
     }
@@ -112,12 +106,14 @@ export default function ChangeIconModal({ shown, close, uploadSuccess }: IModalP
     });
   };
 
-  const listIcons = icons.map((icon) => (
-    <li key={icon.id}>
-      <img src={icon.photo} alt="icon" />
-    </li>
-  ));
-  const [iconCollection, setIconCollection] = useState(false);
+  const getListIcons = useCallback((startIndex?: number, endIndex?: number) => {
+    return icons.slice(startIndex, endIndex)?.map((icon) => (
+      <li key={icon.id}>
+        <img src={icon.photo} alt="icon" />
+      </li>
+    ));
+  }, []);
+
   return shown ? (
     <div className={styles.modalBackdrop} onClick={close} aria-hidden="true">
       <div
@@ -146,7 +142,7 @@ export default function ChangeIconModal({ shown, close, uploadSuccess }: IModalP
                       </button>
                       <h4>Default icons</h4>
                     </div>
-                    <ul>{listIcons}</ul>
+                    <ul>{getListIcons()}</ul>
                   </div>
                 </div>
               ) : (
@@ -167,14 +163,14 @@ export default function ChangeIconModal({ shown, close, uploadSuccess }: IModalP
                             name="Upload a photo"
                             data-testid="picInput"
                             style={{ display: 'none' }}
-                            onChange={uploadFile}
+                            onChange={handleUploadFile}
                           />
                         </label>
                       </div>
                     </div>
                     <div className={styles.photoCollection} style={{ display: 'none' }}>
                       <div className={styles.iconList}>
-                        <ul>{fiveIcons}</ul>
+                        <ul>{getListIcons(0, 5)}</ul>
                       </div>
                       <button type="button" onClick={() => setIconCollection(true)}>
                         <span>
