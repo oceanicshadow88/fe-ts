a50b06ad15542682cc11c5652e5841a36e6dfd83
Chelsea.Y <yangqingyan0@gmail.com>
2025-07-23 14:44:47 +1000

    fix: adjust nested modal

--- src/lib/Modal/Modal.tsx
+++ src/lib/Modal/Modal.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, { createPortal } from 'react-dom';
 import styles from './Modal.module.scss';
 
 interface IModal {
   children?: React.ReactNode;
   classesName?: string;
   fullWidth?: boolean;
+  zIndex?: number;
 }
 export default function Modal({ children, classesName, fullWidth }: IModal) {
   const show = true;
