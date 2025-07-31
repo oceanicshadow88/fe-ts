93e579643dffee14b72af1ff5ddddbec18d2d5df
Chelsea.Y <yangqingyan0@gmail.com>
2025-07-31 12:40:28 +1000

    chore: revert lint ignore

--- src/components/Projects/ProjectEditor/ProjectEditor.tsx
+++ src/components/Projects/ProjectEditor/ProjectEditor.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useContext, useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 import styles from './ProjectEditor.module.scss';
