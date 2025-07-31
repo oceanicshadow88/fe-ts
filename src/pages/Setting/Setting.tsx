8d68d799f7026ac16ac4591623006569a8611836
Chelsea.Y <yangqingyan0@gmail.com>
2025-07-31 12:40:28 +1000

    chore: revert lint ignore

--- src/pages/Setting/Setting.tsx
+++ src/pages/Setting/Setting.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useContext, useEffect, useRef, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { AxiosResponse } from 'axios';
