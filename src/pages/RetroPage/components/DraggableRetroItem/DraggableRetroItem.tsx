c23ccb561e7e89176fb445efa1b7f06437f06daf
Chelsea.Y <yangqingyan0@gmail.com>
2025-08-06 16:35:32 +1000

    feat: add styling to retro board item editor

--- src/pages/RetroPage/components/DraggableRetroItem/DraggableRetroItem.tsx
+++ src/pages/RetroPage/components/DraggableRetroItem/DraggableRetroItem.tsx
@@ -4,6 +4,7 @@
 import React from 'react';
 import { Draggable } from 'react-beautiful-dnd';
 import { GoTrash } from 'react-icons/go';
+import { FiEdit } from 'react-icons/fi';
 import styles from './DraggableRetroItem.module.scss';
 import checkAccess from '../../../../utils/helpers';
 import { Permission } from '../../../../utils/permission';
@@ -31,13 +32,28 @@ export default function DraggableRetroItem(props: IDraggableBoardCard) {
             aria-hidden="true"
             data-testid={`ticket-${item.id}`}
           >
-            <p>{item.content}</p>
+            <span className={styles.editorContainer}>
+              <p>
+                {item.content}
+                <button
+                  aria-label="Edit"
+                  data-testid={`update-retro-item-${item.id}`}
+                  type="button"
+                  className={[styles.iconBtn, styles.editBtn].join(' ')}
+                >
+                  <FiEdit size={14} />
+                </button>
+              </p>
+            </span>
             {checkAccess(Permission.DeleteRetro, projectId) && (
-              <GoTrash
-                onClick={() => onRemoveItem(item.id)}
-                style={{ minWidth: '30px', fontSize: '20px' }}
-                data-testid={`delete-retro-item-${item.id}`}
-              />
+              <button type="button" className={styles.iconBtn}>
+                <GoTrash
+                  onClick={() => onRemoveItem(item.id)}
+                  size={16}
+                  data-testid={`delete-retro-item-${item.id}`}
+                  aria-label="Delete"
+                />
+              </button>
             )}
           </div>
         );
